fad2a33f52cfd67a3b5ae43c6f8011e76c652a95
JolanDUBOIS <jolan11.dubois@gmail.com>
2026-01-29 17:05:59 +0100

    chore: small example for football client

---
+++ examples/football_example.py
@@ -0,0 +1,21 @@
+from sportindex.football import FootballClient
+
+
+if __name__ == "__main__":
+    # Initialize client
+    client = FootballClient()
+
+    # Get all matches for a specific team (e.g., Paris Saint-Germain)
+    matches = client.get_team_fixtures(team_id="psg-263")
+    for match in matches["matches"]:
+        print(f'{match["datetime"]}: {match["home_team"]["name"]} vs {match["away_team"]["name"]}')
+
+    # Get details for a specific match (e.g. Bayer Leverkusen vs PSG)
+    match_details = client.get_match_details(match_id="2636478")
+    print(f'{match_details["datetime"]}: {match_details["home_team"]["name"]} vs {match_details["away_team"]["name"]}')
+    print(f'Competition: {match_details["competition"]["name"]}')
+    for event in match_details.get("extras", {}).get("events", []):
+        if event["name"] in ["Goal", "Penalty"]:
+            team = "Bayer Leverkusen" if event["team"] == "home" else "Paris Saint-Germain"
+            print(f'{event["name"]} scored by {event["extras"]["scorer"]["name"]} at {event["minute"]} minute for {team}')
+
