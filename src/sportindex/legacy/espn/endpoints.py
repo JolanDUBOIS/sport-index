2fcaab568d97e8df3c33296ceb24da2c0377286d
JolanDUBOIS <jolan11.dubois@gmail.com>
2026-02-10 21:26:38 +0100

    chore: removed legacy from repo

--- src/sportindex/legacy/espn/endpoints.py
+++
@@ -1,7 +0,0 @@
-# BASE_URL = "https://site.api.espn.com/apis/site/v2/sports/racing/f1"
-# WEB_BASE_URL = "https://site.web.api.espn.com/apis/v2/sports/racing/f1"
-
-# ENDPOINTS = {
-#     "standings": f"{WEB_BASE_URL}/standings?season={{season}}",
-#     "scoreboard": f"{BASE_URL}/scoreboard?dates={{dates}}",
-# }
